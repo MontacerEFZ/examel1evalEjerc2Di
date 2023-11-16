fc055e7abae3742250909dfec5b6e013b79a1454
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-11-16 10:12:03 +0100

    Initial Commit

---
+++ examel1evalEjerc2DiTests/examel1evalEjerc2DiTests.swift
@@ -0,0 +1,33 @@
+//
+//  examel1evalEjerc2DiTests.swift
+//  examel1evalEjerc2DiTests
+//
+//  Created by Montacer El Fazazi on 16/11/2023.
+//
+
+import XCTest
+@testable import examel1evalEjerc2Di
+
+class examel1evalEjerc2DiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
